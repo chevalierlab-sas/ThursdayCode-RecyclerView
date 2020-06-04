4a3d979924005d4c207bde237e09d0fa74461ba9
aderesie <aderesie.m.s@gmail.com>
2020-06-04 13:36:07 +0700

    Adapter

---
+++ app/src/main/java/lab/chevalier/recyclerviewexample/MyAdapter.kt
@@ -0,0 +1,34 @@
+package lab.chevalier.recyclerviewexample
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import lab.chevalier.recyclerviewexample.databinding.ItemSiswaBinding
+
+class MyAdapter : RecyclerView.Adapter<MyAdapter.SiswaViewHolder>() {
+
+    var listData : List<Siswa> = mutableListOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    inner class SiswaViewHolder(val binding: ItemSiswaBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(data : Siswa){
+            binding.tvNama.text = data.nama
+            binding.tvKelas.text = data.kelas
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SiswaViewHolder {
+        return DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_siswa, parent, false)
+    }
+
+    override fun getItemCount(): Int = listData.size
+
+    override fun onBindViewHolder(holder: SiswaViewHolder, position: Int) {
+        holder.bind(listData[position])
+    }
+
+}
